76fa7263f15269bd376ef131b5fc9f414b966b0a
Prateek Sharma <11prateek12@gmail.com>
2024-10-08 19:49:59 +0530

    Correction in avatar paths

--- server/app.js
+++ server/app.js
@@ -26,12 +26,12 @@ app.use("/api/vehicles", vehicleRouter);
 
 app.get("/api/avatars", (req, res) => {
     const avatars = [
-        "avatars/avatar1.png",
-        "avatars/avatar2.png",
-        "avatars/avatar3.png",
-        "avatars/avatar4.png",
-        "avatars/avatar5.png",
-        "avatars/avatar6.png",
+        "public/avatars/avatar1.png",
+        "public/avatars/avatar2.png",
+        "public/avatars/avatar3.png",
+        "public/avatars/avatar4.png",
+        "public/avatars/avatar5.png",
+        "public/avatars/avatar6.png",
     ];
     res.json(avatars);
 });
