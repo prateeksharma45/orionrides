8affd4c87ce22c4c18d32f86f62b257b144600df
Prateek Sharma <11prateek12@gmail.com>
2024-10-20 18:57:24 +0530

    Modified vercel.json

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import indexRouter from "./routes/index.route.js";
 import vehicleRouter from "./routes/vehicle.route.js";
+import path from "path";
 
 const app = express();
 
@@ -17,8 +18,8 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(cookieParser());
+app.use("/avatars", express.static(path.join(process.cwd(), "public/avatars")));
 
 app.use("/", indexRouter);
 app.use("/api/user", userRouter);
