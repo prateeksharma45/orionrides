5d31797feb137c02614a23376e827dad5fbf8090
Prateek Sharma <11prateek12@gmail.com>
2024-10-29 13:50:28 +0530

    fixed avatar issue

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use("/avatars", express.static("public/avatars"));
 
 app.use("/", indexRouter);
 app.use("/api/user", userRouter);
